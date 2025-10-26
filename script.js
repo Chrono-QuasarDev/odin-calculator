48270010550a98b00bca53fd8d3795d516b8f188
QuasarDev <quasardev9@gmail.com>
2025-10-26 09:59:56 +0000

    feat: Define the fuctions and variables needed for the operations
    The fuctions define the basic math operators we'll use in the calculator
    
    The variables store each part of the operation

---
+++ script.js
@@ -0,0 +1,30 @@
+// ----------------------------------------------------
+// These are the fuctions for the basic math operators we'll use in this calculator
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num1 === 0 || num2 === 0) {
+    return "You’re really testing my patience, aren’t you?"
+  } else {
+    return num1 / num2;
+  }
+}
+
+
+// ----------------------------------------------------
+// These are the variables that'll store the each part of the operation.
+
+let firstNumber = '';
+let operator = null;
+let secondNumber = '';
